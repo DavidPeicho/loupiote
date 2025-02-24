ad76bfed6840548151880ea848ed006a93f5852e
David Peicho <david.peicho@wonderlandengine.com>
2025-02-24 23:24:25 +0100

    js: Cleanup esbuild config

--- loupiote-js/esbuild.mjs
+++ loupiote-js/esbuild.mjs
@@ -1,25 +1,17 @@
 import {build, context} from 'esbuild';
-import {isAbsolute, join, resolve} from 'path';
+import {isAbsolute, join} from 'path';
 import {readFile} from 'fs/promises';
 
-let wasmPlugin = {
+// Inline .wasm imports
+const wasmPlugin = {
     name: 'wasm',
     setup(build) {
-      // Resolve ".wasm" files to a path with a namespace
       build.onResolve({ filter: /\.wasm$/ }, args => {
-        if (args.resolveDir === '') {
-          return // Ignore unresolvable paths
-        }
-        return {
-          path: isAbsolute(args.path) ? args.path : join(args.resolveDir, args.path),
-            namespace: 'wasm-binary',
-        }
+        if (args.resolveDir === '') return;
+        const path = isAbsolute(args.path) ? args.path : join(args.resolveDir, args.path);
+        return { path, namespace: 'wasm-binary' };
       }),
 
-      // Virtual modules in the "wasm-binary" namespace contain the
-      // actual bytes of the WebAssembly file. This uses esbuild's
-      // built-in "binary" loader instead of manually embedding the
-      // binary data inside JavaScript code ourselves.
       build.onLoad({ filter: /.*/, namespace: 'wasm-binary' }, async (args) => ({
         contents: await readFile(args.path),
         loader: 'binary',
